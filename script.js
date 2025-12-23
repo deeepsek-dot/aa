495a52fbfe9d7b2b1ad515123f9c23354f16ece5
deeepsek-dot <deeepsek@gmail.com>
2025-12-23 13:45:46 +0300

    Add image gallery fetching from GitHub API

---
+++ script.js
@@ -0,0 +1,23 @@
+const user = "deeepsek-dot";
+const repo = "AA";
+const folder = "images";
+
+const apiUrl = `https://api.github.com/repos/${user}/${repo}/contents/${folder}`;
+
+fetch(apiUrl)
+  .then(res => res.json())
+  .then(files => {
+    const gallery = document.getElementById("gallery");
+    gallery.innerHTML = "";
+
+    files.forEach(file => {
+      if (file.type === "file" && file.name.match(/\.(jpg|jpeg|png|webp|gif)$/i)) {
+        const img = document.createElement("img");
+        img.src = file.download_url;
+        gallery.appendChild(img);
+      }
+    });
+  })
+  .catch(() => {
+    document.getElementById("gallery").innerText = "Resimler yüklenemedi.";
+  });
